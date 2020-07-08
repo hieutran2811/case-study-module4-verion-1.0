e288ec45c7b13eee2d3ae6e9f07a6355ca0260f9
levanhuan99 <61698248+levanhuan99@users.noreply.github.com>
2020-07-08 17:02:06 +0700

    tạo các packages

---
+++ src/main/java/com/hh/casestudymodule4verion1/CaseStudyModule4Verion1Application.java
@@ -0,0 +1,13 @@
+package com.hh.casestudymodule4verion1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyModule4Verion1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseStudyModule4Verion1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hh/casestudymodule4verion1/CaseStudyModule4Verion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh.casestudymodule4verion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyModule4Verion1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
