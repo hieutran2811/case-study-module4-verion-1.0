b9a754d50347b0cb4e60488b8cc10e4031e75af0
levanhuan99 <61698248+levanhuan99@users.noreply.github.com>
2020-07-13 14:32:56 +0700

    đang làm form log in, thay thế form log in của spring sang của mình

--- src/main/java/com/hh/casestudymodule4verion1/controllers/UserController.java
+++ src/main/java/com/hh/casestudymodule4verion1/controllers/UserController.java
@@ -1,4 +1,33 @@
 package com.hh.casestudymodule4verion1.controllers;
 
+
+import com.hh.casestudymodule4verion1.services.BookService;
+import com.hh.casestudymodule4verion1.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private BookService bookService;
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/")
+    public ModelAndView userHome(@PageableDefault(9)Pageable pageable){
+        ModelAndView modelAndView=null;
+        modelAndView= new ModelAndView("user/home");
+        modelAndView.addObject("bookList",bookService.getAllBook(pageable));
+        modelAndView.addObject("categoryList", categoryService.getAllCategory());
+        return modelAndView;
+
+    }
+
 }
